8e6788a87a6e9a664e30ea7b679034c961d1d930
minhi0449 <minhi0449@gmail.com>
2025-02-13 10:02:49 +0900

    MyBatis 설정

---
+++ src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
@@ -0,0 +1,24 @@
+package com.tenco.bank.repository.interfaces;
+
+import com.tenco.bank.repository.model.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 날짜 : 2025.02.13 (목)
+ * 이름 : 김민희
+ * 내용 :
+ */
+
+// 마이바티스를 만들기 위해서 인터페이스를 만들고 xml 파일을 정의한다
+// 인터페이스 만들고  + xml 파일 정의한다.
+@Mapper // 반드시 선언해 주어야 한다.
+public interface UserRepository {
+    public int insert(User user); // 메서드 이름이랑 xml id
+    public int updateById(User user);
+    public int deleteById(Integer id);
+    public int findById(Integer id);
+    public List<User> findAll();
+
+}
