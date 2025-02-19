f7c090fb3559a3bd2f08e9435a42967e6d2700c9
minhi0449 <minhi0449@gmail.com>
2025-02-19 10:41:45 +0900

    비밀번호 암호화 기능

--- src/main/java/com/tenco/bank/dto/SignUpDTO.java
+++ src/main/java/com/tenco/bank/dto/SignUpDTO.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 /*
   날짜 : 2025.02.13 (목)
   이름 : 김민희
-  내용 :
+  내용 : 회원가입 DTO
+
+  이력 :
  */
 
 
@@ -25,10 +28,14 @@ public class SignUpDTO {
     private String fullname;
 
 //  ToDo 추후 진행 예정
-//	private MultipartFile customFile; // name 속성과 일치 시켜야 함
-//	private String originFileName;
-//	private String uploadFileName;
-//	private String eMail;
+	private MultipartFile customFile; // name 속성과 일치 시켜야 함
+	private String originFileName;
+	private String uploadFileName;
+    // 똑같은 이름이 있다면 ? -> 덮어쓰기 됨
+    // 데이터베이스에서는 사용자가 업로드한 파일을 관리해줄 거임
+
+    // private String eMail;
+
 
     // DTO --> 변환
     public User toUser(){
@@ -36,6 +43,8 @@ public class SignUpDTO {
                 .username(this.username)
                 .password(this.password)
                 .fullname(this.fullname)
+                .uploadFileName(this.uploadFileName)
+                .originFileName(this.originFileName)
                 .build();
     }
 
